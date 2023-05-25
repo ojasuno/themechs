d9d0df4222207f409cb3e262342036372889945b
ojasuno <ojasuno21@gmail.com>
2023-05-25 23:24:49 +1000

    initial commit

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,7 +2,8 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractAddress = "0xed5af388653567af2f388e6224dc7c4b3241c544";
+// export const contractAddress = "0xed5af388653567af2f388e6224dc7c4b3241c544";
+export const contractAddress = "0xb286ac8EFf9F44e2C377c6770CAd5Fc78BFf9eD6";
 
 // The name of the chain your contract is deployed to.
 // Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
